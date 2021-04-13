fd396482001c8ac0506506508bee57ca084738a6
Ju <felixjuliette@gmail.com>
2021-04-13 23:59:25 +0200

    Implémentation de toutes les fonctions de la question 1 et 2 (reste à en tester certaines)

--- projet/main.py
+++ projet/main.py
@@ -14,7 +14,8 @@ def lecture_fichier(nom_fichier):
                 relation[i].append(int(char))
             j += 1
         i += 1
-    relation.remove([])
+    if [] in relation:
+        relation.remove([])
 
     # verification de la taille de la matrice:
     n = len(relation)
@@ -24,57 +25,130 @@ def lecture_fichier(nom_fichier):
 
     return relation
 
+# toutes les fonctions ci dessous renvoient True si la relation vérifie cette propriété
+# et l'indice / les indices d'un contre exemple si la relation ne vérifie pas cette propriété
+
+def reflexive(relation):
+    for x in range(len(relation)):
+        if relation[x][x] != 1:
+            return x
+    return True
+
 
 def symetrie(relation):
-    for i in range(len(relation)):
-        for j in range(len(relation)):
-            if relation[i][j] != relation[j][i]:
-                return False
+    for x in range(len(relation)):
+        for y in range(len(relation)):
+            if relation[x][y] != relation[y][x]:
+                return x, y
     return True
 
 
 def asymetrique(relation):
-    for i in range(len(relation)):
+    for x in range(len(relation)):
         # 0 sur la diagonale
-        if relation[i][i] != 0: return False
-        for j in range(len(relation)):
+        if relation[x][x] != 0:
+            return x, x
+        for y in range(len(relation)):
             # aij = 1 => aji = 0
-            if relation[i][j] == 1 and relation[j][i] == 1:
-                return False
+            if relation[x][y] == 1 and relation[y][x] == 1:
+                return x, y
     return True
 
 
 def transitive(relation):
-    for i in range(len(relation)):
-        for j in range(len(relation)):
-            for k in range(len(relation)):
-                if relation[i][j] == 1 and relation[j][k] == 1 and relation[i][k] == 0:
-                    return False
+    for x in range(len(relation)):
+        for y in range(len(relation)):
+            for z in range(len(relation)):
+                if relation[x][y] == 1 and relation[y][z] == 1 and relation[x][z] == 0:
+                    return x, y, z
+    return True
+
+
+def semi_transitive(relation):
+    for x in range(len(relation)):
+        for y in range(len(relation)):
+            for z in range(len(relation)):
+                for w in range(len(relation)):
+                    if relation[x][y] == 1 and relation[y][z] and (relation[x][z] != 1 and relation[z][w] != 1):
+                        return x, y, z, w
     return True
 
 
 def negtransitive(relation):
-    for i in range(len(relation)):
-        for j in range(len(relation)):
-            for k in range(len(relation)):
-                if relation[i][j] == 0 and relation[j][k] == 0 and relation[i][k] == 1:
-                    return False
+    for x in range(len(relation)):
+        for y in range(len(relation)):
+            for z in range(len(relation)):
+                if relation[x][y] == 0 and relation[y][z] == 0 and relation[x][z] == 1:
+                    return x, y, z
+    return True
+
+
+def ferrer(relation):
+    for x in range(len(relation)):
+        for y in range(len(relation)):
+            for z in range(len(relation)):
+                for w in range(len(relation)):
+                    if relation[x][y] == 1 and relation[y][z] == 1 and (relation[x][w] == 0 and relation[w][z] == 0):
+                        return x, y, z, w
     return True
 
 
 def complete(relation):
-    for i in range(len(relation)):
-        for j in range(len(relation)):
+    for x in range(len(relation)):
+        for y in range(len(relation)):
             # au moins l'un des deux doit etre 1 pour que ce soit complet
-            if relation[i][j] != 0 and relation[j][i] != 0:
-                return False
+            if relation[x][y] != 0 and relation[y][x] != 0:
+                return x, y
     return True
+
+
+def ordre_total(relation):
+    if reflexive(relation) and transitive(relation) and complete(relation) and not symetrie(relation):
+        return True
+    return False
+
+
+def ordre_partiel(relation):
+    if reflexive(relation) and transitive(relation) and not symetrie(relation):
+        return True
+    return False
+
+
+def preodre_partiel(relation):
+    if reflexive(relation) and transitive(relation):
+        return True
+    return False
+
+
+def preordre_total(relation):
+    if reflexive(relation) and transitive(relation) and complete(relation):
+        return True
+    return False
+
+
+def semi_ordre(relation):
+    if reflexive(relation) and complete(relation) and semi_transitive(relation) and ferrer(relation):
+        return True
+    return False
+
+
+def ordre_intervalle(relation):
+    if reflexive(relation) and complete(relation) and ferrer(relation):
+        return True
+    return False
+
 
 
 def main():
     # nom_fichier = input("entrez le nom du fichier (extention comprise) où se trouve la relation: ")
-    nom_fichier = 'exemple.txt'
+    nom_fichier = 'antisym_comp.txt'
     r = lecture_fichier(nom_fichier)
+    print(r)
+    print("comp:", complete(r))
+    print("sym:", symetrie(r))
+    print("asym:", asymetrique(r))
+    print("trans:", transitive(r))
+    print("negtran", negtransitive(r))
 
 
 main()
